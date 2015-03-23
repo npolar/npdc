577fc25de924592b9d5744cc2c30bea9edf381d4
cnrdh <cnrdh@users.noreply.github.com>
2015-03-23 11:29:27 +0100

    _hared

---
+++ app/_shared/model/site.js
@@ -0,0 +1,12 @@
+"use strict"; (function() {
+
+  angular.module("site", []).factory("Site", function($http, Nav, Lang, I18n) {
+    // nav
+    // breadcrumbs
+    // header
+    // footer
+    // lang
+    // user
+  });
+
+})();
