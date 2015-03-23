273c110ca0fd15d8d1cb42bea84151570559824f
cnrdh <cnrdh@users.noreply.github.com>
2015-03-23 11:32:06 +0100

    Login app

---
+++ app/_login/login_controller.js
@@ -0,0 +1,51 @@
+"use strict"; (function() {
+  
+  angular.module("loginApp").controller("LoginController", function ($scope, $http, $location, npolarApiSecurity, User, Nav) {
+    
+    $scope.user = npolarApiSecurity.getUser();
+    
+    $scope.nav = Nav.nav($scope.user);
+  
+    
+    $scope.login = function() {
+      if (!$scope.user.username || !$scope.user.password) {
+        return false;
+      }
+      
+      // Inject new credentials for authInterceptor
+      npolarApiSecurity.setUser($scope.user);
+      
+      // Attempt to fetch user (username/password)
+      var id = encodeURIComponent($scope.user.username).replace(/%20/g,'+');
+      var response = User.fetch({id: id});
+      
+      response.$promise.then(function(user) {
+        $scope.authenticated = true;
+        $scope.error = false;
+        
+        // @todo DON'T do this (yet): $scope.user = response.data.user;
+        $scope.user.name = user.name;
+        npolarApiSecurity.setUser($scope.user);
+        
+      }, function(error) {
+        
+        $scope.error = error;
+        $scope.logout();
+        
+      }); 
+      
+    };
+    
+    $scope.logout = function() {
+      $scope.user = {}
+      $scope.authenticated = false;
+      npolarApiSecurity.removeUser();
+    }
+    
+    if ($scope.user.username && $scope.user.password) {
+      $scope.login();
+    }
+  
+  });
+
+}());
