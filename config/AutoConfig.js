f46652cc2638f4cfa9604671b708b6974417b43b
cnrdh <cnrdh@users.noreply.github.com>
2015-07-17 11:15:42 +0200

    Only allow environment override on localhost

--- config/AutoConfig.js
+++ config/AutoConfig.js
@@ -4,37 +4,53 @@
  */
 var AutoConfig = function(environment) {
   
+  var config = {}; 
+  
+  this.isTest = function(hostname) {
+    return (/test/.test(hostname.split(".")[0]));
+  };
+  
   this.detectEnvironment = function(hostname) {
     var environment;
     if ("localhost" === hostname) {
       environment = "development";
-    } else if ("apptest.data.npolar.no" === hostname) {
+    } else if (this.isTest(hostname)) {
       environment = "test";
     } else if ("api.npolar.no" === hostname) {
       environment = "production";
-    }
+    } 
     return environment;
   };
   
-  var config = {};
+  this.base = function(environment) {
+    var base;
+    if ("development" === environment) {
+      base = "//localhost:9393";
+    } else if ("test" === environment) {
+      base = "//apptest.data.npolar.no";
+    } else if ("production" === environment){
+      base = "//api.npolar.no";
+    } else {
+      throw "Unknown environment: "+environment;
+    }
+    return base;
+  };
   
-  // Auto-detect environment
-  if (window && (environment === undefined || environment === null)) {
+   
+  // Auto-detect environment - only allow overriding on localhost
+  if ("localhost" === window.location.hostname && (typeof environment === 'string' || environment instanceof String)) {
+    console.error("Environment:", environment, "[localhost override]");
+  } else {
     environment = this.detectEnvironment(window.location.hostname);
     console.log("Environment:", environment, "[auto-detected]");
   }
   
+  config.base = this.base(environment);
   config.environment = environment;
-  if ("development" === environment) {
-    config.base = "//localhost:9393";
-  } else if ("test" === environment) {
-    config.base = "//apptest.data.npolar.no";
-  } else {
-    config.base = "//api.npolar.no";
-  }
+  
   
   if ("production" === environment && window) {
-    if ("https:" !== window.location.production) {
+    if ("https:" !== window.location.protocol) {
       console.error("WARNING", "Not using HTTPS in production environment");
     }
     if ("data.npolar.no" !== window.location.hostname) {
