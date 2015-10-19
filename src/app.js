b49c6b9cfc51c4b99c9fdb289d54c7ac58ce253f
RDuX <ruben.dens@gmail.com>
2015-10-19 14:42:10 +0200

    setup

--- src/app.js
+++ src/app.js
@@ -3,6 +3,8 @@ let angular = require('angular');
 let npdcCommon = require('npdc-common');
 let AutoConfig = npdcCommon.AutoConfig;
 
+require('angular-npolar');
+
 var npdc = angular.module('npdc', ['npolarApi', 'npdcUi', 'templates']);
 
 // Auth interceptor
@@ -10,10 +12,30 @@ npdc.config(function ($httpProvider) {
   $httpProvider.interceptors.push('npolarApiInterceptor');
 });
 
+// Bootstrap ngResource models using NpolarApiResource
+var resources = [
+  {'path': '/', 'resource': 'Npdc'},
+  {'path': '/user', 'resource': 'User'}
+];
+
+resources.forEach(function (service) {
+  // Expressive DI syntax is needed here because of the loop..
+  npdc.factory(service.resource, ['NpolarApiResource', function (NpolarApiResource) {
+    return NpolarApiResource.resource(service);
+  }]);
+});
+
+// Routing
+npdc.config(require('./router'));
+
+// Controllers
+npdc.controller('NpdcShowController', require('./show/NpdcShowController'));
+
+// Configure the top bar
+
 npdc.run(function(npolarApiConfig, npdcAppConfig) {
   var environment = "test"; // development | test | production
   var autoconfig = new AutoConfig(environment);
   angular.extend(npolarApiConfig, autoconfig);
-  npdcAppConfig.cardTitle = '';
   npdcAppConfig.toolbarTitle = 'NPDC';
 });
