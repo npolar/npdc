534cb55d24877b24f3fa497cd2319f6551b84d8f
RDuX <ruben.dens@gmail.com>
2015-10-20 10:21:12 +0200

    load latest entries for frontpage items

--- src/show/NpdcShowController.js
+++ src/show/NpdcShowController.js
@@ -1,8 +1,39 @@
 'use strict';
 
 //@ngInclude
-let NpdcShowController = function($scope, npdcAppConfig) {
+let NpdcShowController = function($scope, $http, npdcAppConfig, NpolarApiResource) {
+
   $scope.options = npdcAppConfig;
+  $scope.latest=[];
+
+  let x = [
+    { path: '/dataset'     , params: { 'not-draft': 'yes' } } ,
+    { path: '/map/archive' , params: { 'not-draft': 'yes' } } ,
+    { path: '/expedition'  , params: { 'not-draft': 'yes' } } ,
+    { path: '/publication' , params: { 'not-draft': 'yes' } } ,
+    { path: '/project'     , params: { 'not-draft': 'yes' } } ,
+    { path: '/expedition'  , params:{}}
+  ];
+
+  let resourceFactory = function(path) {
+    let service = { path };
+    return NpolarApiResource.resource(service);
+  };
+
+  const limit = 5;
+  let params = { limit, sort: '-created' };
+
+  let fetchLatest = function() {
+    x.forEach(r => {
+      let R = resourceFactory(r.path);
+      R.array(Object.assign(params, r.params), response => {
+        $scope.latest[r.path.replace('/', '')] = response;
+      });
+    });
+  };
+
+
+  fetchLatest();
 };
 
 module.exports = NpdcShowController;
