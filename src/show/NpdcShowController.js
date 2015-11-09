20e56e9af676458878496e6e0fd259d49b892f11
Remi Aleksander Solås <remi@npolar.no>
2015-11-09 18:00:19 +0100

    Scroll/Resize aesthetics fixed for anchor jumps

--- src/show/NpdcShowController.js
+++ src/show/NpdcShowController.js
@@ -21,71 +21,68 @@ var NpdcShowController = function($scope, $http, $anchorScroll, npdcAppConfig, N
 
 
 // Aestethic page dynamics
-(function(container) {
+(function() {
 	document.addEventListener("DOMContentLoaded", function() {
-		if((container = document.querySelector(container))) {
-			var ease = function(pos, initial, target, len) { return (target - initial) * Math.pow(2.0, 10.0 * (pos / len - 1.0)) + initial; };
-			var header = container.querySelector("header");
-			var toolbar = container.querySelector("md-toolbar");
-			var quicknav = header.querySelector(".quicknav");
-			var pagenav = container.querySelector(".pagenav");
-			var boxShadow = window.getComputedStyle(toolbar).boxShadow;
-			var scrollUp = container.querySelector("#scroll-up");
+		var boxShadow = window.getComputedStyle(document.querySelector(".npdc-home md-toolbar")).boxShadow;
+
+		function ease(pos, initial, target, len) {
+			return (target - initial) * Math.pow(2.0, 10.0 * (pos / len - 1.0)) + initial;
+		};
+
+		function updateCallback() {
+			var container   = document.querySelector(".npdc-home");
+			var header      = container.querySelector("header");
+			var toolbar     = container.querySelector("md-toolbar");
+			var quicknav    = header.querySelector(".quicknav");
+			var pagenav     = container.querySelector(".pagenav");
+			var scroller    = container.querySelector("#scroll-up");
+
+			var scrollY     = window.pageYOffset, scale = 1.0;
 
 			var pageSets = [
 				{
 					section: document.getElementById("news"),
-					link: pagenav.querySelectorAll("a")[0]
+					link: document.querySelectorAll(".pagenav a")[0]
 				},
 				{
 					section: document.getElementById("expeditions"),
-					link: pagenav.querySelectorAll("a")[1]
+					link: document.querySelectorAll(".pagenav a")[1]
 				},
 				{
 					section: document.getElementById("documents"),
-					link: pagenav.querySelectorAll("a")[2]
+					link: document.querySelectorAll(".pagenav a")[2]
 				}
 			];
 
-			toolbar.style.boxShadow = "none";
-			header.style.boxShadow = boxShadow;
+			if(container.offsetWidth > 700) {
+				(scrollY && (scale = Math.max(0.0, ease(scrollY, 1.0, 0.0, header.offsetHeight - toolbar.offsetHeight))));
+				quicknav.style.transform = "translateY(calc(-50% + " + (scrollY / 2) + "px)) scale(" + scale + ")";
+			} else {
+				quicknav.style.transform = "none";
+			}
 
-			var updateCallback = function() {
-				var scrollY = window.pageYOffset, scale = 1.0;
+			if(scrollY >= header.offsetHeight - toolbar.offsetHeight) {
+				header.style.boxShadow = "none";
+				toolbar.style.boxShadow = boxShadow;
+				pagenav.classList.add("docked");
+				scroller.style.transform = "scale(1.0)";
+			} else {
+				header.style.boxShadow = boxShadow;
+				toolbar.style.boxShadow = "none";
+				pagenav.classList.remove("docked");
+				scroller.style.transform = "scale(0.0)";
+			}
 
-				if(container.offsetWidth > 700) {
-					if(scrollY) {
-						scale = Math.max(0.0, ease(scrollY, 1.0, 0.0, header.offsetHeight - toolbar.offsetHeight));
-					}
-
-					quicknav.style.transform = "translateY(calc(-50% + " + (scrollY / 2) + "px)) scale(" + scale + ")";
-
-				} else {
-					quicknav.style.transform = "none";
-				}
-
-				if(scrollY >= header.offsetHeight - toolbar.offsetHeight) {
-					header.style.boxShadow = "none";
-					toolbar.style.boxShadow = boxShadow;
-					pagenav.classList.add("docked");
-					scrollUp.style.transform = "scale(1.0)";
-				} else {
-					header.style.boxShadow = boxShadow;
-					toolbar.style.boxShadow = "none";
-					pagenav.classList.remove("docked");
-					scrollUp.style.transform = "scale(0.0)";
-				}
-
-				pageSets.forEach(function(set, index, arr) {
-					set.link.style.transform = ((scrollY >= set.section.offsetTop - toolbar.offsetHeight) ? "scale(0.0)" : "");
-				});
-			};
-
-			window.addEventListener("scroll", updateCallback);
-			window.addEventListener("resize", updateCallback);
+			pageSets.forEach(function(set, index, arr) {
+				set.link.style.transform = ((scrollY >= set.section.offsetTop - toolbar.offsetHeight) ? "scale(0.0)" : "");
+			});
 		}
+
+		window.addEventListener("scroll", updateCallback);
+		window.addEventListener("resize", updateCallback);
+		updateCallback();
 	});
-})(".npdc-home");
+})();
 
 
 module.exports = NpdcShowController;
