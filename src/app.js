c137026b4514ed9d58297a0a844241ca861ad231
Remi Aleksander Solås <remi@npolar.no>
2015-11-09 17:59:21 +0100

    Code refactoring for consistency

--- src/app.js
+++ src/app.js
@@ -1,26 +1,26 @@
-'use strict';
-let angular = require('angular');
-let npdcCommon = require('npdc-common');
-let AutoConfig = npdcCommon.AutoConfig;
+"use strict";
 
-require('angular-npolar');
+var angular     = require("angular");
+var npdcCommon  = require("npdc-common");
+var AutoConfig  = npdcCommon.AutoConfig;
 
-var npdc = angular.module('npdcHome', ['ngRoute', 'npolarUi', 'npolarApi', 'npdcUi', 'templates']);
+require("angular-npolar");
+
+var npdc = angular.module("npdcHome", ["ngRoute", "npolarUi", "npolarApi", "npdcUi", "templates"]);
 
 // Auth interceptor
 npdc.config(function ($httpProvider) {
-  $httpProvider.interceptors.push('npolarApiInterceptor');
+  $httpProvider.interceptors.push("npolarApiInterceptor");
 });
 
 // Routing
-npdc.config(require('./router'));
+npdc.config(require("./router"));
 
 // Controllers
-npdc.controller('NpdcShowController', require('./show/NpdcShowController'));
-npdc.controller('GlobalSearchController', require('./search/GlobalSearchController'));
+npdc.controller("NpdcShowController", require("./show/NpdcShowController"));
+npdc.controller("GlobalSearchController", require("./search/GlobalSearchController"));
 
-// Configure the top bar
-
+// App configurations
 npdc.run(function(npolarApiConfig, npdcAppConfig) {
   var environment = "production"; // development | test | production
   var autoconfig = new AutoConfig(environment);
