eec15070e0a0474afaf291e666610fd59e87bf4a
Anders Bälter <anders.balter@npolar.no>
2016-02-12 15:58:33 +0100

    work work

--- src/show/NpdcShowController.js
+++ src/show/NpdcShowController.js
@@ -1,9 +1,16 @@
 "use strict";
 
-var NpdcShowController = function($scope, $location, NpdcSearchService, NpdcAutocompleteConfigFactory) {
+var NpdcShowController = function($scope, $location, npdcAppConfig, NpdcSearchService, NpdcAutocompleteConfigFactory) {
 	'ngInject';
 
 	$scope.sections = [{},{}];
+  $scope.apps = npdcAppConfig.toolbarMenuGroups
+    .reduce((memo, group) => memo.concat(group.apps), [])
+    .map(app => {
+      app.img = app.img.replace('48', '96');
+      return app;
+  });
+
 	$scope.searchOptions = new NpdcAutocompleteConfigFactory({ global: true, showCollections: false, floatingLabel: false });
 
   $scope.search = function(q) {
