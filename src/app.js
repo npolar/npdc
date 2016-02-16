a0f6b634751e420f7499cce32d212259a9d61fcf
cnrdh <cnrdh@users.noreply.github.com>
2016-02-16 11:44:23 +0100

    Added i18n and start of geo integration, removed auth interceptor, added resources

--- src/app.js
+++ src/app.js
@@ -6,10 +6,7 @@ var AutoConfig  = npdcCommon.AutoConfig;
 
 var npdc = angular.module("npdcHome", ["npdcCommon"]);
 
-// Auth interceptor
-npdc.config(function ($httpProvider) {
-  $httpProvider.interceptors.push("npolarApiInterceptor");
-});
+// No auth interceptor
 
 // Routing
 npdc.config(require("./router"));
@@ -18,11 +15,43 @@ npdc.config(require("./router"));
 npdc.controller("NpdcShowController", require("./show/NpdcShowController"));
 npdc.controller("GlobalSearchController", require("./search/GlobalSearchController"));
 
+npdc.value('NpdcApplications', require('npdc-common/src/config/npdc-applications.json'));
+
+// Bootstrap ngResource models using NpolarApiResource
+var resources = [
+  { path: '/service', 'resource': 'Service', cache: true, base: '//api.npolar.no' },
+  { path: '/arcgis/rest/services', 'resource': 'ArcGIS', cache: true, base: '//geodata.npolar.no' },
+];
+resources.forEach(function (service) {
+  // Expressive DI syntax is needed here
+  npdc.factory(service.resource, ['NpolarApiResource', function (NpolarApiResource) {
+    return NpolarApiResource.resource(service);
+  }]);
+});
+
 // App configurations
-npdc.run(function(npolarApiConfig, npdcAppConfig) {
+npdc.run(function($http, npolarApiConfig, npdcAppConfig, NpolarTranslate, NpolarLang, ArcGIS) {
   var environment = "production"; // development | test | production
-  var autoconfig = new AutoConfig(environment);
-  angular.extend(npolarApiConfig, autoconfig);
+  Object.assign(npolarApiConfig, new AutoConfig(environment));
+  
+  // i18n
+  $http.get('//api.npolar.no/text/?q=&filter-bundle=npdc-home&format=json&variant=array&limit=all').then(response => {
+    NpolarTranslate.appendToDictionary(response.data);
+    NpolarLang.setLanguages(npdcAppConfig.i18n.languages);
+  });
+  
+  // http://geodata.npolar.no/arcgis/rest/services/?f=pjson
+  // http://geodata.npolar.no/arcgis/rest/services/inspire1?f=pjson
+  ArcGIS.get({id: 'inspire1', f: 'pjson'}).$promise.then(f => {
+    console.debug(f);
+  });
+  // Map apps =>
+  // Svalbardkartet
+  // Barentsportal
+  // Topo?
+  
+  
+  
 });
 
 // Datetime filter
